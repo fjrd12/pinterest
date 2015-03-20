5315c535293686a3faa64cce194159893bf2f40e
Francisco Rodriguez <fjrd12@gmail.com>
2015-03-20 15:15:23 -0600

    pins views

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!
  
   def index
     @pins = Pin.all
@@ -12,7 +13,7 @@ class PinsController < ApplicationController
 
 
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
   def edit
@@ -20,15 +21,13 @@ class PinsController < ApplicationController
 
 
   def create
-    @pin = Pin.new(pin_params)
-
-    respond_to do |format|
+    @pin = current_user.pins.build(pin_params)
+  
       if @pin.save
         redirect_to @pin, notice: 'Pin was successfully created.' 
       else
         render :new 
       end
-    end
   end
 
   def update
@@ -41,8 +40,7 @@ class PinsController < ApplicationController
 
   def destroy
     @pin.destroy
-   
-      redirect_to pins_url
+    redirect_to pins_url
 
   end
 
@@ -50,6 +48,12 @@ class PinsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
       @pin = Pin.find(params[:id])
+    end
+
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Usuario no autorizado" if @pin.nil?
+
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
