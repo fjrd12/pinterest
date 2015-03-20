6776fbeaf7c93c0f74b227338f280c4565cd55a7
Francisco Rodriguez <fjrd12@gmail.com>
2015-03-20 12:16:15 -0600

    relationships

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150320004802) do
+ActiveRecord::Schema.define(version: 20150320181500) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
     t.string   "string"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.string   "index"
   end
 
   create_table "users", force: :cascade do |t|
