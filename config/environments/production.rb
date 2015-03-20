e42bf1b38d38886f011cbbf690b296843e12133e
Francisco Rodriguez <fjrd12@gmail.com>
2015-03-20 17:32:19 -0600

    s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,17 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   # Required for Heroku
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  
+# Set amazon credentials
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :key => "value", 
+    s3_credentials => 
+        {
+        :bucket => ENV['AWS_BUCKET'],
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+        }
+  }
+
 end
